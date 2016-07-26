9bf4d5073108305499152e0520880c6ac39863e7
Vad <vad.adler@gmail.com>
2016-07-26 11:08:28 -0700

    Implemented merge sort algorithm.

--- iq/src/sorting/MergeSort.java
+++ iq/src/sorting/MergeSort.java
@@ -1,25 +1,37 @@
 package sorting;
 
+import java.util.Arrays;
+
 /**
  * Divide and conquer algorithm.
  * 
  * Recursively divide array in halves until arrays contain 1 element.
  * Merge resulting arrays.
  * 
- * Worse case time complexity O(nlog(n)) 
- *
+ * Time complexity O(nlog(n)) 
  */
 public class MergeSort {
   int[] arr;
   int[] tmp;
   
-   public MergeSort(int[] data) {
-     arr = data;
-     tmp = new int[arr.length];
-   }
+  public static void main(String[] args) {
+  	int[] a = { 1,4,5,2,8,9};
+  	
+  	System.out.println("Array before sort: " + Arrays.toString(a));
+  	
+  	MergeSort ms = new MergeSort(a);
+  	ms.sort();
+  	
+  	System.out.println("Array after sort: " + Arrays.toString(a));
+  }
+   
+  public MergeSort(int[] data) {
+  	arr = data;
+    tmp = new int[arr.length];
+  }
    
    public void sort() {
-     mergesort(0, arr.length - 1);
+  	 mergesort(0, arr.length - 1);
    }
    
    public void mergesort(int low, int high) {
@@ -38,15 +50,30 @@ public class MergeSort {
        tmp[i] = arr[i];
      }
      
-     int i = low;
-     int k = middle + 1;
-     int j = high;
+     int left = low;
+     int right = middle + 1;
+     int current = left;
      
      // Copy smallest value from either side into original array.
-     while(i <= middle && j <= high) {
-       if(tmp[i] > tmp[j]) {
-         
+     while(left <= middle && right <= high) {
+       if(tmp[left] <= tmp[right]) {	// Left element is smaller.
+         arr[current] = tmp[left];
+         left++;
        }
+       else {													// Right element is smaller.
+      	 arr[current] = tmp[right];
+      	 right++;
+       }
+       current++;
      }
+     
+     // Copy the rest of the left side.
+     int remaining = middle - left;
+     for(int i = 0; i <= remaining; i++) {
+    	 arr[current + i] = tmp[left + 1];
+     }
+     
+     // There is no need to copy the rest of the right side.
+     // They are already there.
    }
 }
