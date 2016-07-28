d342d140f8326e67cbd6c937e9e9aab19b18eb9c
Vad <vad.adler@gmail.com>
2016-07-28 16:07:51 -0700

    Modified MergeSort's merge method.

--- iq/src/sorting/MergeSort.java
+++ iq/src/sorting/MergeSort.java
@@ -15,7 +15,7 @@ public class MergeSort {
   int[] tmp;
   
   public static void main(String[] args) {
-  	int[] a = {1,4,5,2,8,7};
+  	int[] a = {5,1,4,2};
   	
   	System.out.println("Array before sort: " + Arrays.toString(a));
   	
@@ -41,8 +41,11 @@ public class MergeSort {
     * @param high end of the array.
     */
    public void mergeSort(int low, int high) {
-     // Check if low is smaller then high, if not then the array is sorted.
-     if(low < high) {
+     if(low == high) {
+    	 return;	// Single element array. No need to sort.
+     }
+     else {
+    	 System.out.println("mergeSort: " + low + ":" + high);
        int middle = (low + high) / 2;
        mergeSort(low, middle);        // Sort left side.
        mergeSort(middle + 1, high);   // Sort right side.
@@ -57,42 +60,65 @@ public class MergeSort {
     * @param high end of right array.
     */
    public void merge(int low, int middle, int high) {
+  	 System.out.println("low=" + low + " middle=" + middle + " high=" + high);
      // Copy data into helper array.
      for(int i = low; i <= high; i++) {
        tmp[i] = arr[i];
      }
      
-     System.out.println(Arrays.toString(tmp));
-     
      int left = low;
      int right = middle + 1;
-     int current = left;
+     int current = low;	// Index into temp array.
      
      // Copy smallest value from either side into original array.
      while(left <= middle && right <= high) {
        if(tmp[left] <= tmp[right]) {	// Left element is smaller.
-         arr[current] = tmp[left];
-         left++;
+         arr[current++] = tmp[left++];
        }
        else {													// Right element is smaller.
-      	 arr[current] = tmp[right];
-      	 right++;
+      	 arr[current++] = tmp[right++];
        }
-       current++;
+     }
+     
+     // Copy the rest of the left side. Right side is already sorted due to
+     // the fact that left side is shorter or equal to the right side. 
+     // When merging two arrays starting with the smaller array (left) by the
+     // time merging logic runs out of left elements the right array will be sorted.
+     int remaining = middle - left;
+     System.out.println("remaining=" + remaining + " middle=" + middle + " left=" + left);
+     for(int i = 0; i <= remaining; i++) {
+    	 arr[current + i] = tmp[left + i];
+     }
+   }   
+   
+   public void merge2(int low, int middle, int high) {
+     int left = low;
+     int right = middle + 1;
+     int current = low;	// Index into temp array.
+     
+     // Copy smallest value from either side into original array.
+     while(left <= middle && right <= high) {
+       if(tmp[left] <= tmp[right]) {	// Left element is smaller.
+         tmp[current++] = arr[left++];
+       }
+       else {													// Right element is smaller.
+      	 tmp[current++] = arr[right++];
+       }
      }
      
      // Copy the rest of the left side.
-     int remaining = middle - left;
-     for(int i = 0; i < remaining; i++) {
-    	 arr[current + i] = tmp[left + i];
+     while(left <= middle) {
+    	 tmp[current++] = arr[left++];
      }
      
      // Copy the rest of the right side.
-     remaining = high - middle -1;
-     //for(int i = 0; i < remaining; i++) {
-    //	 arr[current + i] = tmp[left + 1];
-     //}
-
-     //System.out.println(Arrays.toString(arr));
+     while(middle <= high) {
+    	 tmp[current++] = arr[middle++];
+     }
+     
+     // Copy elements from temp array.
+     for(int i = 0; i < (high - low + 1); i++) {
+    	 arr[low + i] = tmp[i];
+     }
    }
 }
