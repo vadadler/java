b55b93029280589fe4a73cebadf5a8cb7fa7720b
Vad <vad.adler@gmail.com>
2016-08-02 16:27:44 -0700

    Added PairSum
    Also code refactoring.

---
+++ iq/src/arrays/PairSum.java
@@ -0,0 +1,97 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Given an array of integers find all unique
+ * pairs which sum up to a value.
+ * 
+ * Three possible solutions:
+ * 	1. Brute force. 
+ * 		a. Nested loops to check all possible combinations.
+ * 		b. Using hash table to store elements as keys and 
+ * 			 (sum - element) as value.
+ *  2. Sort array. Binary search for (sum - element).
+ *
+ */
+
+public class PairSum {
+	public static void main(String[] args) {
+		int[] a = {5,3,7,0,1,2,4};
+		int sum = 5;
+		
+		System.out.println(Arrays.toString(a));
+		
+		PairSum ps = new PairSum();
+		System.out.println("Using nested loops");
+		ps.pairSumNestedLoops(a, sum);
+
+		System.out.println("Using hashmap trick");
+		ps.pairSumHashMapDiff(a, sum);
+
+		System.out.println("Using sorted array and binary search");
+		ps.pairSumSortBinary(a, sum);
+}
+	
+	/**
+	 * Brute force method. Check all pairs (double nested loops).
+	 * 
+	 * Time complexity O(n^2)
+	 * 
+	 * @param arr
+	 * @param sum
+	 */
+	private void pairSumNestedLoops(int[] arr, int sum) {
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = i + 1; j < arr.length; j++) {
+				if(arr[i] + arr[j] == sum) {
+					System.out.println("[" + arr[i] + "," + arr[j] + "] = " + sum);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Use a hash table to store the difference between sum and each of the elements 
+	 * in the array. Loop through the array, check each element against the hash table.
+	 * 
+	 * NOTE: current implementation does not deal with duplicate entries. I.e. 
+	 * [5,0] and [0,5] are treated as different pair.
+	 * 
+	 * Time complexity: O(N)
+	 * 
+	 * @param arr
+	 * @param sum
+	 */
+	private void pairSumHashMapDiff(int[] arr, int sum) {
+		HashMap<Integer, Integer> hmDiff = new HashMap<Integer, Integer>();
+		
+		// Populate hashmap.
+		for(int i = 0; i < arr.length; i++) {
+			hmDiff.put(i, sum - arr[i]);
+		}
+		
+		// Loop through input array and check values with hashmap. 
+		for(int i = 0; i < arr.length; i++) {
+			int value = hmDiff.get(i);
+			if(hmDiff.containsKey(value) == true) {
+				System.out.println("[" + arr[i] + "," + value + "] = " + sum);
+			}
+		}
+	}
+	
+	private void pairSumSortBinary(int[] arr, int sum) {
+		// Sort array.
+		Arrays.sort(arr);
+		
+		searching.BinarySearch bs = new searching.BinarySearch(arr);
+		
+		for(int i = 0; i < arr.length; i++) {
+			int index = bs.findIndex(sum - arr[i]);
+			if(index >= 0) {
+				System.out.println("[" + arr[i] + "," + arr[index] + "] = " + sum);
+			}
+		}
+	}
+}
--- iq/src/co/Yelp.java
+++ iq/src/co/Yelp.java
@@ -1,5 +1,49 @@
 package co;
 
-public class Yelp {
+/**
+ * List of technical interview questions asked by Yelp.
+ */
 
+public class Yelp {
+	
+	/**
+	 * Write a function to compare two strings and return true if anagrams.
+	 */
+	
+	/**
+	 * Read in N Strings and determine if they are anagrams of each other.
+	 * 	'cat','act','tac' -> true
+	 *  'cat', 'bat', 'act' -> false
+	 */
+	
+	/**
+	 * Given a string of words, reverse the words. Ex: "I like cats" --> "cats like I"
+	 */
+
+	/**
+	 * Given a string how would you check if the string is a palindrome?
+	 */
+	
+	/**
+	 * Question: Search complexity in a sorted and unsorted array.
+	 * Answer: Time complexity for sorted array is O(Log(N)) for
+	 *         unsorted array O(N)  
+	 */
+	
+	/**
+	 * Question: How many bits in an Octal number.
+	 * Answer: Octal numbers are from 0 to 7 = 8 == 2 ^ 3 = 3 bits  
+	 */
+	
+	/**
+	 * How would you reverse a linked list?
+	 */
+	
+	/**
+	 * Given a list l and an int k produce a list of distinct pairs that add up to k 
+	 */
+	
+	public static void main(String[] args) {
+		
+	}
 }
