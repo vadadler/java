baac7e9cccc9c88c1a202f89499636713ee70452
Vad <vad.adler@gmail.com>
2016-08-10 16:01:12 -0700

    Updates to some data structures.

--- iq/src/datastructures/Stack.java
+++ iq/src/datastructures/Stack.java
@@ -1,26 +1,54 @@
 package datastructures;
 
+import java.util.ArrayList;
+
 /**
  * Stack uses LIFO ordering.
  */
 public class Stack {
-  LinkedList data = new LinkedList();
+	int mSize = -1;
+  ArrayList<Integer> mData = new ArrayList<Integer>();
   
   public void push(int v) {
-    data.add(data.new Node(v));
+  	mData.add(v);
+  	mSize++;
   }
   
-  public int pop() {
-    int value = data.head.value;
-    data.delete(data.head.value);
-    return value;
+  public Integer pop() {
+  	if(isEmpty() == false) {
+  		int value = peek();
+  		mData.remove(mSize--);
+  		return value;
+  	}
+    return null;
   }
   
-  public int peek() {
-    return data.head.value;
+  public Integer peek() {
+    return mData.get(mSize - 1);
   }
   
-  public boolean isEmpty() {
-    return data.size() == 0 ? true : false;
+  public Boolean isEmpty() {
+    return mData.size() == 0 ? true : false;
+  }
+  
+  public void print() {
+  	for(int i = mData.size() - 1; i >= 0 ; i--) {
+  		System.out.println(mData.get(i));
+  	}
+  }
+  
+  public static void main(String[] args) {
+  	Stack s = new Stack();
+  	s.push(0);
+  	s.push(1);
+  	s.push(2);
+  	s.push(3);
+  	s.push(4);
+  	s.print();
+  	
+  	System.out.println("After 2 pushes");
+  	s.pop();
+  	s.pop();
+  	s.print();
   }
 }
