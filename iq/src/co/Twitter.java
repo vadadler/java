01b3d23089e842038bafa82175a8945119332349
Vad <vad.adler@gmail.com>
2016-08-10 18:31:55 -0700

    Added twitter class.

---
+++ iq/src/co/Twitter.java
@@ -0,0 +1,15 @@
+package co;
+
+public class Twitter {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * Mouse and cheese trap question: There is a grid, and mouse is in 
+	 * one coordinate. He has to reach the cheese. There are mousetraps 
+	 * and walls, means he mouse cannot go to those grids. Find the 
+	 * smallest path for the mouse to reach cheese
+	 */
+}
