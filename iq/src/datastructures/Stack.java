2d9034c1727bb1fb20d4a81ee5249b7866fbd6aa
Vad <vad.adler@gmail.com>
2016-08-10 17:03:01 -0700

    Added min property to Stack.

--- iq/src/datastructures/Stack.java
+++ iq/src/datastructures/Stack.java
@@ -8,16 +8,35 @@ import java.util.ArrayList;
 public class Stack {
 	int mSize = -1;
   ArrayList<Integer> mData = new ArrayList<Integer>();
+
+  // Helper data structure to keep min value for each
+  // corresponding value in the main data structure.
+  ArrayList<Integer> mMinData = new ArrayList<Integer>();
   
   public void push(int v) {
   	mData.add(v);
+  	
+  	if(mMinData.isEmpty() == true) {
+  		mMinData.add(v);
+  	}
+  	else {
+  		int minValue = mMinData.get(mSize); 
+  		if(v < minValue) {
+  			mMinData.add(v);
+  		}
+  		else {
+  			mMinData.add(minValue);
+  		}
+  	}
+  	
   	mSize++;
   }
   
   public Integer pop() {
   	if(isEmpty() == false) {
   		int value = peek();
-  		mData.remove(mSize--);
+  		mData.remove(mSize);
+  		mMinData.remove(mSize--);
   		return value;
   	}
     return null;
@@ -27,28 +46,36 @@ public class Stack {
     return mData.get(mSize - 1);
   }
   
+  public Integer getMin() {
+  	return mMinData.get(mSize);
+  }
+  
   public Boolean isEmpty() {
     return mData.size() == 0 ? true : false;
   }
   
   public void print() {
   	for(int i = mData.size() - 1; i >= 0 ; i--) {
-  		System.out.println(mData.get(i));
+  		System.out.println(mData.get(i) + " " + mMinData.get(i));
   	}
   }
   
   public static void main(String[] args) {
+  	
   	Stack s = new Stack();
-  	s.push(0);
-  	s.push(1);
+  	s.push(5);
+  	s.push(7);
+  	s.push(4);
   	s.push(2);
   	s.push(3);
-  	s.push(4);
   	s.print();
   	
-  	System.out.println("After 2 pushes");
+  	System.out.println("Min=" + s.getMin());
+  	
+  	System.out.println("After 2 pops");
   	s.pop();
   	s.pop();
   	s.print();
+  	System.out.println("Min=" + s.getMin());
   }
 }
