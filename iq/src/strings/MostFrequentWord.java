02c1a9eb4646bf64cdf2bd1c876f0621d50b905e
Vad <vad.adler@gmail.com>
2016-08-18 08:43:06 -0700

    Unfinished code.

---
+++ iq/src/strings/MostFrequentWord.java
@@ -0,0 +1,17 @@
+package strings;
+
+/**
+ * Given a string, return an word or words repeated the most.
+ * 
+ * Asked: Meraki
+ *
+ */
+public class MostFrequentWord {
+   public static void main(String[] args) {
+      MostFrequentWord mfq = new MostFrequentWord();
+   }
+   
+   private String getMostFrequentWord(String inStr) {
+      return "";
+   }
+}
