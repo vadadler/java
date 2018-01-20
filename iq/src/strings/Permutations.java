835fc57988cbf5b713b16bdbe18f68b8d689ed47
vad <vad.adler@gmail.com>
2018-01-20 13:32:20 -0800

    Added another string permutation method.

--- iq/src/strings/Permutations.java
+++ iq/src/strings/Permutations.java
@@ -16,6 +16,8 @@ public class Permutations {
 
     System.out.println("is " + one + " a permutation of " + two +  " ? " + isPermutation(one, two));
 
+    p.permutate(one, "");
+
   }
     
   private void perm(String prefix, String str) {
@@ -30,6 +32,43 @@ public class Permutations {
         perm(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, len));
       }
     }
+  }
+
+  private void permutate(String src, String chosen) {
+     //System.out.println("permutation(" + src + ", " + chosen + ")");
+     if(src.length() == 0) {
+        System.out.println(chosen);
+     }
+     else {
+        StringBuilder sbSrc = new StringBuilder(src);
+        StringBuilder sbChosen = new StringBuilder(chosen);
+
+        // Looping through all letters in src.
+        for(int i = 0; i < src.length(); i++) {
+           // Choose.
+
+           Character c = src.charAt(i);
+           // Remove character from i position.
+           sbSrc.deleteCharAt(i);
+
+           // Add character to the end of already chosen chars.
+           sbChosen.append(c);
+
+           // Explore.
+           permutate(sbSrc.toString(), sbChosen.toString());
+
+           // Unchoose/Undo. Put back as it was before.
+
+           // Insert character at i position.
+           sbSrc.insert(i, c);
+
+           // Delete last character, added when we did choose.
+           sbChosen.deleteCharAt(sbChosen.length() - 1);
+
+           //src = sbSrc.toString();
+           //chosen = sbChosen.toString();
+        }
+     }
   }
 
   /**
